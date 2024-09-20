d84346abae16931924c86e0eea6df92e0f0b4c01
Felipe Kamakura <felipedea+odspmdb@microsoft.com>
2024-09-20 11:17:36 -0700

    Introduce Config abstraction

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,26 @@ use std::{env, fs::read_to_string};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_args(&args);
+    let config = Config::new(&args);
   
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = read_to_string(file_path).expect("Should have read the file successfully!");
+    let contents = read_to_string(config.file_path).expect("Should have read the file successfully!");
 
     println!("With text:\n{contents}");
 }
 
-fn parse_args(args: &Vec<String>) -> (&String, &String) {
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    let query = &args[1];
-    let file_path = &args[2];
-
-    (query, file_path)
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Config { query, file_path }
+    }
 }
