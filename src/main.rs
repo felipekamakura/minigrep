a14fafae1a3a020624229e2f383805f03339638a
Felipe Kamakura <felipedea+odspmdb@microsoft.com>
2024-09-20 10:56:29 -0700

    reading sample file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::env;
+use std::{env, fs::read_to_string};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     let query = &args[1];
     let file_path = &args[2];
 
     println!("Searching for {query}");
     println!("In file {file_path}");
+
+    let contents = read_to_string(file_path).expect("Should have read the file successfully!");
+
+    println!("With text:\n{contents}");
 }
